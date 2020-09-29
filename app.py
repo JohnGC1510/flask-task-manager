eff7823dab279104135e8b692f9936680460b87c
JohnGC1510 <johngc1510@gmail.com>
2020-09-29 17:20:27 +0000

    Added a index to task names and categoreis and the functionallity to search through the index to filter results

--- app.py
+++ app.py
@@ -27,6 +27,17 @@ def get_tasks():
     return render_template("tasks.html", tasks=tasks)
 
 
+@app.route("/search", methods=["GET", "POST"])
+def search():
+    query = request.form.get("query")
+    """ $text $search means peform a search other a text index from this
+    collection we created a index to allow searching through tasks
+     names and descriptions
+    """
+    tasks = list(mongo.db.tasks.find({"$text": {"$search": query}}))
+    return render_template("tasks.html", tasks=tasks)
+
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
